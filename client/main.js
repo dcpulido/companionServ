a9c0b1b55ec638c087ab5f540a6e5357e899096c
dcpulido <dcpulido91@gmail.com>
2017-03-20 14:11:07 +0100

    1.0

--- client/main.js
+++ client/main.js
@@ -1,8 +1,11 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
-import veh from '../imports/components/vehicle/vehicle';
+import vehi from '../imports/components/vehicle/vehicle';
+import 'bootstrap/dist/css/bootstrap.css';
  
+
 angular.module('simple-app', [
     angularMeteor,
+    'ngSanitize',
     vehicle.name
 ]);
--- imports/components/vehicle/vehicle.js
+++ imports/components/vehicle/vehicle.js
@@ -5,18 +5,28 @@ import { Person } from '../../api/person.js';
 import { Travel } from '../../api/travel.js';
 var onTravel=[];
 var totalTrav=[];
+var drivers=[];
+var driver=null;
 class vehicleCtrl {
-  constructor($scope) {
-    $scope.viewModel(this);
-    this.helpers({
-        persons(){
-            return Person.find({});
-        },
-        travels(){
-            return Travel.find({}); 
-        }
-    })
-  }
+    constructor($scope) {
+        this.scope=$scope;
+        $scope.viewModel(this);
+        $scope.myhtml="hola";
+        this.helpers({
+            persons(){
+                return Person.find({});
+            },
+            travels(){
+                console.log("from trav"+drivers);
+                return Travel.find({},{
+                    sort: {
+                        createdAt: -1
+                    }
+                }); 
+            }
+            
+        })
+    }
     addPerson(newPerson,newCar) {
         // Insert a task into the collection
         console.log(newPerson);
@@ -46,78 +56,189 @@ class vehicleCtrl {
 
         this.newCar = '';
         this.newPerson = '';
-  }
+    }
     setChecked(person) {
-    // Set the checked property to the opposite of its current value
-    if(!person.checked){
-        onTravel.push(person);
-        console.log(onTravel);
-    }
-    if(person.checked){
-        var aux=[];
-        for(i=0;i<onTravel.length;i++){
-            console.log(onTravel[i]);
-            if (onTravel[i]._id != person._id){
-                aux.push(onTravel[i]);
-            }
+        // Set the checked property to the opposite of its current value
+        if(!person.checked){
+            onTravel.push(person);
+            console.log(onTravel);
         }
-        onTravel=aux;
-        console.log(onTravel);
+        if(person.checked){
+            var aux=[];
+            for(i=0;i<onTravel.length;i++){
+                console.log(onTravel[i]);
+                if (onTravel[i]._id != person._id){
+                    aux.push(onTravel[i]);
+                }
+            }
+            onTravel=aux;
+            console.log(onTravel);
+        }
+        Person.update(person._id, {
+          $set: {
+            checked: !person.checked
+          },
+        });
     }
-    Person.update(person._id, {
-      $set: {
-        checked: !person.checked
-      },
-    });
-  }
- 
-  removePerson(person) {
-    Person.remove(person._id);
-  }
-    addTravel(){
-        var history=[];
-        
+    dri(){
+        return drivers;
+    }
+    removePerson(person) {
+        Person.remove(person._id);
+    }
+    addTravel(){     
         let trav =Travel.find({}).fetch();
-        console.log(trav);
+        console.log("Todos los viajes: "+trav);
+        var history=this.getHistoryForGroup(trav);
+        var count=this.getCountForDrivers(history);
+        console.log("count");
+        for(i=0;i<count.length;i++){
+            console.log(count[i].name+" "+count[i].count);
+        }
+        var recomended=this.getRecomendedDrivers(count);
+        console.log("recomended");
+        for(i=0;i<recomended.length;i++){
+            console.log(recomended[i].name+" "+recomended[i].count);
+        }
+        
+        console.log(this.showResponse(recomended));
+        
+        this.scope.response = this.showResponse(recomended);
+        
+    }
+    removeTravel(travel) {
+        Travel.remove(travel._id);
+    }
+    getHistoryForGroup(trav){
+        console.log("getHistoryForGroup");
         var aux=0;
+        var history=[];
+        //se recorren todos los viajes
         for(i=0;i<trav.length;i++){
             var flog=true;
+            //se comprueba longitud si no hay el mismo numero de pasajeros es q ya no es igual
             if(trav[i].passengers.length==onTravel.length){
+                //se recorren los pasajeros
                 for(h=0;h<trav[i].passengers.length;h++){
                     var flig=false;
+                    //se recorren pasajeros de viaje actual
                     for(k=0;k<onTravel.length;k++){
+                        //se comprueba q sean los mismos
                         if(onTravel[k].name==trav[i].passengers[h].name){
                             flig=true;
                         }
-                    } 
+                    }
                 }
+            }else{
+                flog=false;
             }
             if(flig==false){
                 flog=false;
             }
-            else{
+            if(flog==true){
                 history.push(trav[i]);
                 aux++;
             }
         }
-        console.log(aux);
-        
-        
-        
-        Travel.insert({
-          driver: onTravel[0],
-          passengers: onTravel,
-          createdAt: new Date
-        });
+        console.log(history);
+        console.log("getHistoryForGroup/////");
+        return history;
         
     }
-    removeTravel(travel) {
-    Travel.remove(travel._id);
-  }
+    getCountForDrivers(hist){
+        console.log("getCountForDrivers");
+        var count=[];
+        //damos formato a count para posteriormente llevar la cuenta de cada uno
+        if(hist.length>0){
+            for(i=0;i<hist[0].passengers.length;i++){
+                count.push({name:hist[0].passengers[i].name,count:0});
+            }
+            //recorremos el historial de viajes para este grupo
+            for(i=0;i<hist.length;i++){
+                for(k=0;k<count.length;k++){
+                    if(hist[i].driver.name==count[k].name){
+                        count[k].count=count[k].count+1;
+                    }
+                }
+            }   
+        }
+        console.log("getCountForDrivers/////");
+        return count;
+    }
+    getRecomendedDrivers(count){
+        console.log("getRecomendedDrivers");
+        var aux=999999;
+        var toret=[];
+        if(count.length>0){
+            for(i=0;i<count.length;i++){
+                if(count[i].count<aux){
+                    aux=count[i].count;
+                }
+            }
+            for(i=0;i<count.length;i++){
+                if(count[i].count==aux){
+                    toret.push(count[i]);
+                }
+            }
+        }
+        console.log("getRecomendedDrivers/////");
+        return toret;
+    }
+    showResponse(dd){
+        console.log("showResponse");
+        var str="";
+        str+="<ul>";
+        str+="<li>";
+        
+        for(i=0;i<dd.length;i++){
+            //str+="<button class='btn btn-evento' ng-click=''>Guardar!</button>";
+            str+="<span class='text'>";
+            str+=    dd[i].name +"  "+ dd[i].count;
+            str+="</span>";
+            str += "<br>";
+        }
+        
+        str+="</li>";
+        str+="</ul>";
+      
+        console.log("showResponse////7");
+        return str;
+        
+    }
+    setAsDriver(person){
+        alert(person.name+" set as driver");
+        var str="";
+        str+="<ul>";
+        str+="<li>";
+        str+="<span class='text'>";
+        str+=    person.name +" set as driver";
+        str+="</span>";
+        str += "<br>";
+        str+="</li>";
+        str+="</ul>";
+        driver=person;
+        this.scope.driv = str;
+        
+    }
+    saveTravel(){
+        console.log("save");
+        if(driver!=null){
+            Travel.insert({
+              driver: driver,
+              passengers: onTravel,
+              createdAt: new Date
+            });
+            driver=null;
+        }
+        else{
+            alert("driver set as null");
+        }
+        console.log("save////");
+    }
 }
  
 export default angular.module('simple-app', [
-  angularMeteor
+  angularMeteor,'ngSanitize'
 ])
   .component('vehicle', {
     templateUrl: 'imports/components/vehicle/vehicle.html',
